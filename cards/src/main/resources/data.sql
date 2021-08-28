37aa24382528acc46d949bab39ff63976da8da48
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-08-28 17:12:18 -0300

    Criado Dockerfile para MS Accounts
    - Criado Dockerfile para MS Accounts
    - Ajustada formatação dos arquivos de data
    - Adicionada propriedade para permitir acesso ao H2 dentro do docker

--- cards/src/main/resources/data.sql
+++ cards/src/main/resources/data.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS cards;
-
 CREATE TABLE `cards` (
   `card_id` int NOT NULL AUTO_INCREMENT,
   `card_number` varchar(100) NOT NULL,
@@ -11,13 +10,57 @@ CREATE TABLE `cards` (
   `create_dt` date DEFAULT NULL,
   PRIMARY KEY (`card_id`)
 );
-
-
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('4565XXXX4656', 1, 'Credit', 10000, 500, 9500, CURDATE());
-
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('3455XXXX8673', 1, 'Credit', 7500, 600, 6900, CURDATE());
- 
-INSERT INTO `cards` (`card_number`, `customer_id`, `card_type`, `total_limit`, `amount_used`, `available_amount`, `create_dt`)
- VALUES ('2359XXXX9346', 1, 'Credit', 20000, 4000, 16000, CURDATE());
+INSERT INTO `cards` (
+    `card_number`,
+    `customer_id`,
+    `card_type`,
+    `total_limit`,
+    `amount_used`,
+    `available_amount`,
+    `create_dt`
+  )
+VALUES (
+    '4565XXXX4656',
+    1,
+    'Credit',
+    10000,
+    500,
+    9500,
+    CURDATE()
+  );
+INSERT INTO `cards` (
+    `card_number`,
+    `customer_id`,
+    `card_type`,
+    `total_limit`,
+    `amount_used`,
+    `available_amount`,
+    `create_dt`
+  )
+VALUES (
+    '3455XXXX8673',
+    1,
+    'Credit',
+    7500,
+    600,
+    6900,
+    CURDATE()
+  );
+INSERT INTO `cards` (
+    `card_number`,
+    `customer_id`,
+    `card_type`,
+    `total_limit`,
+    `amount_used`,
+    `available_amount`,
+    `create_dt`
+  )
+VALUES (
+    '2359XXXX9346',
+    1,
+    'Credit',
+    20000,
+    4000,
+    16000,
+    CURDATE()
+  );
