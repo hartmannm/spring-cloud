8464bae844d39ac8c4328cb72599fea6c761c712
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-09-01 22:02:14 -0300

    Configurações centralizadas
    Adaptado MS Cards para ler as configurações do projeto centralizado

--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -1,20 +1,29 @@
 package com.eazybytes.cards.controller;
 
+import java.util.List;
+
+import com.eazybytes.cards.config.CardsServiceConfig;
 import com.eazybytes.cards.model.Cards;
 import com.eazybytes.cards.model.Customer;
+import com.eazybytes.cards.model.Properties;
 import com.eazybytes.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CardsController {
 
     @Autowired
     private CardsRepository cardsRepository;
+    @Autowired
+    private CardsServiceConfig cardsServiceConfig;
 
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -24,6 +33,15 @@ public class CardsController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/card/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(cardsServiceConfig.getMsg(), cardsServiceConfig.getBuildVersion(),
+                cardsServiceConfig.getMailDetails(), cardsServiceConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
